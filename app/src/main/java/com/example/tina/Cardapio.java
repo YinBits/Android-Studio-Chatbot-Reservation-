b4b6dd2536944697d393656b2569ce861eaf7328
Bruno <brunomarcaldossantos9a@gmail.com>
2023-08-24 21:36:56 -0300

    Cardápio funcional

--- app/src/main/java/com/example/tina/Cardapio.java
+++ app/src/main/java/com/example/tina/Cardapio.java
@@ -7,58 +7,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cardapio#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Cardapio extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Cardapio() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cardapio.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Cardapio newInstance(String param1, String param2) {
-        Cardapio fragment = new Cardapio();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_cardapio, container, false);
+
+        Button buttonPratos = rootView.findViewById(R.id.btn_pratos);
+        buttonPratos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(new Pratos());
+            }
+        });
+
+        Button buttonPorcoes = rootView.findViewById(R.id.btn_porcoes);
+        buttonPorcoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(new Porcoes());
+            }
+        });
+
+        Button buttonPizzas = rootView.findViewById(R.id.btn_pizzas);
+        buttonPizzas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(new Pizzas());
+            }
+        });
+
+        Button buttonBebidas = rootView.findViewById(R.id.btn_bebidas);
+        buttonBebidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(new Bebidas());
+            }
+        });
+
+        Button buttonDoces = rootView.findViewById(R.id.btn_doces);
+        buttonDoces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(new Doces());
+            }
+        });
+
+        return rootView;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cardapio, container, false);
+    private void openFragment(Fragment fragment) {
+        requireActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment) // assuming 'fragment_container' is the container for your fragments
+                .addToBackStack(null)
+                .commit();
     }
 }
---
+++ app/src/main/java/com/example/tina/Porcoes.java
@@ -0,0 +1,42 @@
+package com.example.tina;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class Porcoes extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_pratos, container, false);
+
+        // Configurações e lógica específicas do PratosFragment
+
+        Button buttonVoltarCardapio = rootView.findViewById(R.id.btn_voltar);
+        buttonVoltarCardapio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCardapio();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void openCardapio() {
+        Cardapio cardapio = new Cardapio();
+        requireActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, cardapio) // assuming 'fragment_container' is the container for your fragments
+                .addToBackStack(null)
+                .commit();
+    }
+}
