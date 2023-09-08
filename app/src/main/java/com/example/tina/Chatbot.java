f8e55a770d12ed838261d44f00597ecca443554f
Bruno <brunomarcaldossantos9a@gmail.com>
2023-09-08 17:40:59 -0300

    Chatbot com problema no "uma da tarde""

--- app/src/main/java/com/example/tina/Chatbot.java
+++ app/src/main/java/com/example/tina/Chatbot.java
@@ -14,9 +14,9 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
 
 public class Chatbot extends Fragment {
 
@@ -108,16 +108,13 @@ public class Chatbot extends Fragment {
             // Armazenar a resposta do usuário nas variáveis apropriadas com base na pergunta atual
             String currentQuestion = questions.get(currentQuestionIndex).toLowerCase(); // Converter para letras minúsculas
 
-            // Extrair apenas números da mensagem do usuário
-            String extractedNumbers = extractNumbers(userMessage);
-
             // Com base na pergunta atual, atualize as variáveis de reserva com as respostas do usuário
             if (currentQuestion.contains("horário")) {
-                reservationTime = extractedNumbers;
+                reservationTime = convertWordToNumber(userMessage);
             } else if (currentQuestion.contains("quantas pessoas")) {
-                numberOfSeats = extractedNumbers;
+                numberOfSeats = convertWordToNumber(userMessage);
             } else if (currentQuestion.contains("número da mesa")) {
-                tableNumber = extractedNumbers;
+                tableNumber = convertWordToNumber(userMessage);
             }
 
             // Continue com a próxima pergunta
@@ -126,16 +123,90 @@ public class Chatbot extends Fragment {
         }
     }
 
-    // Função para extrair apenas números de uma string
-    private String extractNumbers(String input) {
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(input);
-        StringBuilder extractedNumbers = new StringBuilder();
-        while (matcher.find()) {
-            extractedNumbers.append(matcher.group());
+    // Função para converter palavras em números
+    private String convertWordToNumber(String input) {
+        // Defina um mapeamento de palavras para números
+        Map<String, Integer> wordToNumberMap = new HashMap<>();
+
+        //Número de Mesas
+        wordToNumberMap.put("um", 1);
+        wordToNumberMap.put("uma", 1);
+        wordToNumberMap.put("dois", 2);
+        wordToNumberMap.put("duas", 2);
+        wordToNumberMap.put("três", 3);
+        wordToNumberMap.put("quatro", 4);
+        wordToNumberMap.put("cinco", 5);
+        wordToNumberMap.put("seis", 6);
+        wordToNumberMap.put("sete", 7);
+        wordToNumberMap.put("oito", 8);
+        wordToNumberMap.put("nove", 9);
+        wordToNumberMap.put("dez", 10);
+        wordToNumberMap.put("onze", 11);
+        wordToNumberMap.put("doze", 12);
+        wordToNumberMap.put("treze", 13);
+        wordToNumberMap.put("catorze", 14);
+        wordToNumberMap.put("quatorze", 14);
+        wordToNumberMap.put("quinze", 15);
+        wordToNumberMap.put("dezesseis", 16);
+        wordToNumberMap.put("dezessete", 17);
+        wordToNumberMap.put("meio-dia", 12);
+        wordToNumberMap.put("meia-noite", 24);
+        wordToNumberMap.put("meio dia", 12);
+        wordToNumberMap.put("meia noite", 24);
+        // Adicione mais números conforme necessário
+
+        // Expressões de hora por extenso
+        String[] hourExpressions = {
+                "uma",
+                "duas",
+                "três",
+                "quatro",
+                "cinco",
+                "seis",
+                "sete",
+                "oito",
+                "nove",
+                "dez",
+                "onze",
+                "doze",
+                "meio dia",
+                "meia noite",
+                "meio-dia",
+                "meia-noite",
+        };
+
+        // Adicione mapeamentos para horas por extenso
+        for (int i = 0; i < hourExpressions.length; i++) {
+            wordToNumberMap.put(hourExpressions[i] + " hora da manhã", i + 1);
+            wordToNumberMap.put(hourExpressions[i] + " da manhã", i + 1);
+            wordToNumberMap.put(hourExpressions[i] + " hora da tarde", i + 13); // Horas da tarde começam em 13
+            wordToNumberMap.put(hourExpressions[i] + " da tarde", i + 13); // Horas da tarde começam em 13
+            wordToNumberMap.put(hourExpressions[i] + " hora da noite", i + 19); // Horas da noite começam em 19
+            wordToNumberMap.put(hourExpressions[i] + " da noite", i + 19); // Horas da noite começam em 19
         }
-        return extractedNumbers.toString();
+
+        // Tente converter a entrada diretamente para um número
+        try {
+            int number = Integer.parseInt(input);
+            return String.valueOf(number);
+        } catch (NumberFormatException e) {
+            // Se não for um número válido, tente procurar no mapeamento de palavras
+            String[] words = input.split("\\s+");
+            int result = 0;
+
+            for (String word : words) {
+                String lowercaseWord = word.toLowerCase();
+
+                if (wordToNumberMap.containsKey(lowercaseWord)) {
+                    result += wordToNumberMap.get(lowercaseWord);
+                }
+            }
+
+            return String.valueOf(result);
+        }
     }
+
+
 
     private void addBotMessage(String sender, String message) {
         // Adicione a mensagem do bot ao chat
