f5a5bec054ea6d493ae0b057454238b9d4babe00
Bruno <brunomarcaldossantos9a@gmail.com>
2023-11-01 09:31:29 -0300

    Backup

--- app/src/main/java/com/example/tina/CalendarHelper.java
+++
@@ -1,9 +0,0 @@
-package com.example.tina;
-
-import java.util.List;
-
-public class CalendarHelper {
-    public static List<CalendarEvent> getEvents(int year, int month, int dayOfMonth) {
-        return null;
-    }
-}
